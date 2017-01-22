1cfac3244881b380cd38dd8a7174d626186d051c
Stefan Bunk <stefan.bunk@googlemail.com>
2017-01-22 01:31:33 +0100

    Basic trip layout

--- simple_vbb.py
+++ simple_vbb.py
@@ -1,14 +1,27 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from simple_vbb.vbb import Vbb
 
 app = Flask(__name__)
 vbb = Vbb()
 
+FROM = "S Nikolassee"
+
+FROM = "S Wannsee"
+TO = "U Stadtmitte (Berlin)"
+FROM_ID = vbb.get_station_ext_id(FROM)
+TO_ID = vbb.get_station_ext_id(TO)
+
 
 @app.route("/")
 def root():
-    return render_template("main.html")
+    text = vbb.get_trip(FROM_ID, TO_ID)
+    return render_template("main.html", text=text)
+
+
+@app.route("/trips")
+def trips():
+    trips = vbb.get_trip(FROM_ID, TO_ID)
+    return render_template("trips.html", trips=trips)
 
 if __name__ == "__main__":
     app.run()
--- simple_vbb/vbb.py
+++ simple_vbb/vbb.py
@@ -1,27 +1,41 @@
 import json
 import requests
 
+ACCESS_KEY_FILE = 'access_key.txt'
+
 
 class Vbb:
 
     def __init__(self):
-        pass
+        try:
+            self.access_key = self._read_access_key()
+        except:
+            raise Exception('Expecting access key file %s' % ACCESS_KEY_FILE)
+        self.base_request = {
+            'accessId': self.access_key,
+            'lang': 'en',
+            'format': 'json',
+        }
 
-    def foo(self):
-        return "Hallo Welt"
-
-    def read_access_key(self):
+    def _read_access_key(self):
         with open('access_key.txt', 'r') as f:
             return f.read().replace('\n', '')
 
-    def demo(self):
-        params = {
-            'accessId': self.read_access_key(),
-            'lang': 'en',
-            'format': 'json',
-
-            'input': 'S Wannsee'
-        }
+    def get_station_ext_id(self, search_str):
+        params = self.base_request.copy()
+        params["input"] = search_str
         r = requests.get('http://demo.hafas.de/openapi/vbb-proxy/location.name', params=params)
         j = json.loads(r.text)
-        print(json.dumps(j, indent=4, sort_keys=True))
+        station_ext_id = j["stopLocationOrCoordLocation"][0]["StopLocation"]["extId"]
+        return station_ext_id
+
+    def get_trip(self, from_ext_id, to_ext_id):
+        params = self.base_request.copy()
+        params["originExtId"] = from_ext_id
+        params["destExtId"] = to_ext_id
+
+        r = requests.get('http://demo.hafas.de/openapi/vbb-proxy/trip', params=params)
+        with open("debug.json", "w") as f:
+            json.dump(r.json(), f)
+        return r.json()["Trip"]
+        # return json.dumps(r.json()["Trip"], indent=4)
